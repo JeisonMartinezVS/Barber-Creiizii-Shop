ee4c508ad6656b676cbd42f716d6112de17f9264
JeisonMartinezVS <jeison.martinez@virtualsoft.tech>
2025-11-07 23:07:08 -0500

    Creacion de configuracion

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -8,13 +8,13 @@ import { getFirestore } from 'firebase/firestore'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyBBZ1qQeounICe9xC9NTbVNqK3hcOxdrxc',
-  authDomain: 'barber-creiizii-shop.firebaseapp.com',
-  projectId: 'barber-creiizii-shop',
-  storageBucket: 'barber-creiizii-shop.firebasestorage.app',
-  messagingSenderId: '439116492645',
-  appId: '1:439116492645:web:3a3fe22e9baf899f6e7b8c',
-  measurementId: 'G-BNR9QWM97R',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 }
 
 // Initialize Firebase
